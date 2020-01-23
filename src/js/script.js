3ba618694ad586aad4c05b21fbb1c08a30935fc4
Nadja Brandys <nadiusza@gmail.com>
2020-01-23 20:29:36 +0100

    Add cart class and toggle fuction

--- src/js/script.js
+++ src/js/script.js
@@ -4,6 +4,7 @@
 const select = {
   templateOf: {
     menuProduct: '#template-menu-product',
+    cartProduct: '#template-cart-product', // CODE ADDED
   },
   containerOf: {
     menu: '#product-list',
@@ -24,30 +25,61 @@ const select = {
   },
   widgets: {
     amount: {
-      input: 'input[name="amount"]',
+      input: 'input.amount', // CODE CHANGED
       linkDecrease: 'a[href="#less"]',
       linkIncrease: 'a[href="#more"]',
     },
   },
+  // CODE ADDED START
+  cart: {
+    productList: '.cart__order-summary',
+    toggleTrigger: '.cart__summary',
+    totalNumber: `.cart__total-number`,
+    totalPrice: '.cart__total-price strong, .cart__order-total .cart__order-price-sum strong',
+    subtotalPrice: '.cart__order-subtotal .cart__order-price-sum strong',
+    deliveryFee: '.cart__order-delivery .cart__order-price-sum strong',
+    form: '.cart__order',
+    formSubmit: '.cart__order [type="submit"]',
+    phone: '[name="phone"]',
+    address: '[name="address"]',
+  },
+  cartProduct: {
+    amountWidget: '.widget-amount',
+    price: '.cart__product-price',
+    edit: '[href="#edit"]',
+    remove: '[href="#remove"]',
+  },
+  // CODE ADDED END
 };
-
 const classNames = {
   menuProduct: {
     wrapperActive: 'active',
     imageVisible: 'active',
   },
+  // CODE ADDED START
+  cart: {
+    wrapperActive: 'active',
+  },
+  // CODE ADDED END
 };
-
 const settings = {
   amountWidget: {
     defaultValue: 1,
     defaultMin: 1,
     defaultMax: 9,
-  }
+  }, // CODE CHANGED
+  // CODE ADDED START
+  cart: {
+    defaultDeliveryFee: 20,
+  },
+  // CODE ADDED END
 };
 
 const templates = {
   menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
+  // CODE ADDED START
+  cartProduct: Handlebars.compile(document.querySelector(select.templateOf.cartProduct).innerHTML),
+  // CODE ADDED END
 };
 
 class Product {
@@ -97,7 +129,6 @@ class Product {
     const clickedElement = thisProduct.accordionTrigger;
     /* START: click event listener to trigger */
     clickedElement.addEventListener('click', function (event) {
-      console.log('clicked element', clickedElement);
       /* prevent default action for event */
       event.preventDefault();
       /* toggle active class on element of thisProduct */
@@ -254,6 +285,27 @@ class AmountWidget {
   }
 }
 
+class Cart {
+  constructor(element) {
+    const thisCart = this;
+    thisCart.products = [];    
+    thisCart.getElements(element);
+    thisCart.initActions();
+    console.log('new cart', thisCart);
+  }
+  getElements(element) {
+    const thisCart = this;
+    thisCart.dom = {};
+    thisCart.dom.wrapper = element;
+    thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger);
+}
+  initActions(){
+    const thisCart = this;
+    thisCart.dom.toggleTrigger.addEventListener('click', function(){
+      thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
+    });
+}
+}
 const app = {
   initMenu: function () {
     const thisApp = this;
@@ -269,6 +321,12 @@ const app = {
     thisApp.data = dataSource;
   },
 
+  initCart: function () {
+    const thisApp = this;
+    const cartElem = document.querySelector(select.containerOf.cart);
+    thisApp.cart = new Cart(cartElem);
+  },
+
   init: function () {
     const thisApp = this;
     console.log('*** App starting ***');
@@ -280,7 +338,10 @@ const app = {
     thisApp.initData();
 
     thisApp.initMenu();
+
+    thisApp.initCart();
   },
 };
 app.init();
+
 
