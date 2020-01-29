3d11e803e1e7616f04227789c17ff4458ba84db4
Nadja Brandys <nadiusza@gmail.com>
2020-01-29 23:14:34 +0100

    10.2 Add subpage functionality

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,64 @@
 
-import{settings, select} from './settings.js';
+import { settings, select, classNames } from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
 
 
 const app = {
+
+  initPages: function () {
+    const thisApp = this;
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks=document.querySelectorAll(select.nav.links);
+    const idFromHash = window.location.hash.replace('#/', '');
+
+let pageMatchingHash = thisApp.pages[0].id;
+
+    for(let page of thisApp.pages){
+      if(page.id == idFromHash){
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+    thisApp.activatePage(pageMatchingHash);
+
+    for (let link of thisApp.navLinks) {
+      link.addEventListener('click', function (event) {
+        const clickedElement = this;
+        event.preventDefault();
+
+        /*get page id from href attribute */
+        const id = clickedElement.getAttribute('href').replace('#', '');
+        /*this thisApp.activatePage with that id */
+        thisApp.activatePage(id);
+        /*change url hash */
+        window.location.hash = '#/'+ id;
+      });
+    }
+  },
+  activatePage: function (pageId) {
+    const thisApp = this;
+    /*add class active to matching pages remove from non-matching */
+    for (let page of thisApp.pages) {
+      // if (page.id==pageId){
+      //   page.classList.add(classNames.pages.active);
+      // }else{
+      //   page.classList.remove(classNames.pages.active);
+      // }
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+    }
+    /*add class active to matching LINKS, remove from non-matching */
+    for (let link of thisApp.navLinks) {
+      link.classList.toggle(
+        classNames.nav.active,
+        link.getAttribute('href') == '#' + pageId
+      );
+    }
+  },
   initMenu: function () {
     const thisApp = this;
     console.log('thisApp.data', thisApp.data);
-    for (let productData in thisApp.data.products) {   //nowa instancja dla kazdego produktu
+    for (let productData in thisApp.data.products) {
       new Product(thisApp.data.products[productData].id, thisApp.data.products[productData]);
     }
   },
@@ -39,7 +89,7 @@ const app = {
     const cartElem = document.querySelector(select.containerOf.cart);
     thisApp.cart = new Cart(cartElem);
     thisApp.productList = document.querySelector(select.containerOf.menu);
-    thisApp.productList.addEventListener('add-to-cart', function(event){
+    thisApp.productList.addEventListener('add-to-cart', function (event) {
       app.cart.add(event.detail.product);
     });
   },
@@ -50,6 +100,7 @@ const app = {
     thisApp.initData();
 
     thisApp.initCart();
+    thisApp.initPages();
   },
 };
 app.init();
